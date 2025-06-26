06ccb4cba177cf4d91d52b77dff9313edb4a4863
JustManiak <116386054+JustManiak@users.noreply.github.com>
2025-06-27 02:44:03 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,285 @@
+# -*- coding: utf-8 -*
+import inspect
+from ctypes import memmove
+# -*- coding: utf-8 -*
+# -*- coding: utf-8 -*
+
+from math import log
+import math
+import inspect
+import dis
+# This program demonstrates various Python features and syntax
+
+# Basic variable assignment and string manipulation
+my_name = "Alice"
+q = 1337
+print(f"Hello, {my_name}!")
+print(log(2.0, 3.0))
+print(math.pow(2, 3))
+
+# String concatenation
+a = "Hello"
+b = "World"
+print(a + b)
+print("Goodbye" + "World")
+v = a[1] + b[2]
+print(v)
+
+p = 1337
+seed_int_var_bdabeeaacf = 2782
+i = 1000
+seed_int_var_eeabdbadbe = 5046
+j = 7777
+
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+v = list1[0] + list2[1]
+print(v)
+
+# Lists and list comprehensions
+my_list = [1, 2, 3, 4, 5]
+squared_list = [x**2 for x in my_list]
+print(squared_list)
+
+# Conditional statements and loops
+for l in range(10):
+    if l % 2 == 0:
+        print(f"{l} is even")
+    else:
+        print(f"{l} is odd")
+seed_int_var_addfccdaac = 6724
+
+# Functions and recursion
+def factorial(n, seed_int_param_efcdcadedb, seed_int_param_daeddeeebf, seed_int_param_fcacfacacc, seed_int_param_aeccdecaee, seed_int_param_ccbfdfffda, seed_int_param_aebcfecafa, seed_int_param_aefffdcace, seed_int_param_bdbbcdbcdc):
+    seed_int_var_ddcabdeeaf = 3378
+    seed_int_var_fbdaabaaad = 5998
+    seed_int_var_dfaefbbddf = 9158
+    seed_int_var_befedcffec = 3670
+    seed_int_var_acdceccced = 1095
+    seed_int_var_aeaedbcbbe = 4481
+    seed_int_var_eccbbdadbe = 1546
+    seed_int_var_cccdeabbbd = 6066
+    seed_int_var_efecafffbc = 2717
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1, 32, 91, 47, 41, 8, 8, 98, 100)
+print(factorial(5, 7, 97, 7, 49, 36, 73, 46, 79))
+
+
+# Classes and inheritance
+class Animal:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+        seed_int_var_aceeddabab = 7592
+
+    def JiIĲǉIⅉiĳÎljĴĲĲjｊｊljiḬIǉｊǉｊÎIḬ(self):
+        seed_int_var_dbaffaeaab = 5051
+        seed_int_var_deedfbadea = 905
+        seed_int_var_ffbbebface = 4467
+        print("Animal sound")
+        seed_int_var_fdfdedccac = 7917
+
+class Dog(Animal):
+    def __init__(self, name, color, breed):
+        super().__init__(name, color)
+        self.breed = breed
+        seed_int_var_ccedaaeaad = 5104
+        seed_int_var_facfdecbfe = 5995
+        seed_int_var_ecdcdaacfa = 904
+
+    def JiIĲǉIⅉiĳÎljĴĲĲjｊｊljiḬIǉｊǉｊÎIḬ(self):
+        seed_int_var_cdcaeadfaf = 5995
+        seed_int_var_bacdeeddbf = 7814
+        seed_int_var_decdaafdde = 2619
+        seed_int_var_feecbcceba = 3642
+        seed_int_var_badfdcafef = 6460
+        seed_int_var_dcdddeebdc = 9203
+        seed_int_var_ecaabbbdcb = 4612
+        seed_int_var_ccacaddafc = 1786
+        print("Woof!")
+        return "Bark!"
+        seed_int_var_faaaaecdfe = 3164
+
+class Cat(Animal):
+    def __init__(self, name, color):
+        seed_int_var_bafaacebdd = 5315
+        seed_int_var_fabaeafbce = 6481
+        seed_int_var_edbcacdfbc = 2042
+        super().__init__(name, color)
+        seed_int_var_deaefcdadc = 9009
+
+    def JiIĲǉIⅉiĳÎljĴĲĲjｊｊljiḬIǉｊǉｊÎIḬ(self):
+        seed_int_var_dbbefadecb = 4165
+        seed_int_var_abdfafbacb = 1759
+        seed_int_var_daaabcebfb = 1573
+        seed_int_var_ecdfabeeac = 9653
+        seed_int_var_eedcaefcbf = 2491
+        seed_int_var_ecededbfdf = 5458
+        seed_int_var_fcabddaebb = 5288
+        seed_int_var_cdcecfdffe = 6927
+        seed_int_var_fcfeedcefe = 6836
+        print("Meow!")
+        seed_int_var_ffacecdfad = 9078
+        seed_int_var_beeaddfeda = 712
+
+class Person:
+    def __init__(self, name):
+        seed_int_var_fbdbeaaeea = 7432
+        seed_int_var_aeacfbdfdc = 3516
+        self.name = name
+        self.pets = []
+
+    def JiⅉiJḬIｊǉｊĴÎiIĴIljillÎIḬǉⅉｊjǉⅉ(self, pet):
+        seed_int_var_ebfffcaeac = 9752
+        seed_int_var_ccfeadaedc = 6095
+        seed_int_var_cbbbbadbdc = 2187
+        seed_int_var_dbbeeacedb = 39
+        seed_int_var_fcffcceeda = 7124
+        seed_int_var_dffdffbfbd = 8976
+        seed_int_var_deeecdfcdf = 7788
+        seed_int_var_dffaebeeab = 3692
+        seed_int_var_edfebafddb = 6746
+        seed_int_var_cedabeafab = 6019
+        seed_int_var_cedfbeffab = 8392
+        seed_int_var_bdbdeabbfe = 3355
+        seed_int_var_afeceabeaf = 1472
+        seed_int_var_cadbaeafee = 1788
+        seed_int_var_aeccacccec = 3654
+        self.pets.append(pet)
+
+    def ĴǉĲlijJḬⅉJĲlijIḬｊJḬǉⅉĴḬｊĳĴḬĲJI(self):
+        seed_int_var_aaeaccbdcd = 8412
+        seed_int_var_bdcdadffba = 6397
+        for pet in self.pets:
+            print(pet.name, pet.color, pet.__class__.__name__)
+
+my_dog = Dog("Fido", "brown", "Golden Retriever")
+seed_int_var_debdfadedd = 449
+my_cat = Cat("Whiskers", "gray")
+my_person = Person("Bob")
+my_person.JiⅉiJḬIｊǉｊĴÎiIĴIljillÎIḬǉⅉｊjǉⅉ(my_dog)
+my_person.JiⅉiJḬIｊǉｊĴÎiIĴIljillÎIḬǉⅉｊjǉⅉ(my_cat)
+my_person.ĴǉĲlijJḬⅉJĲlijIḬｊJḬǉⅉĴḬｊĳĴḬĲJI()
+
+def square_list(n, seed_int_param_defffdfecc, seed_int_param_aebbbbeeab, seed_int_param_aabcbaaacf, seed_int_param_fbbedcaaee, seed_int_param_eedbdecaeb, seed_int_param_fbeceaeaae, seed_int_param_bdebacaabf, seed_int_param_efdbcdfdfd):
+    seed_int_var_fffdafdbfd = 5399
+    seed_int_var_aaadbfccae = 4967
+    seed_int_var_dfbfdcaefe = 4438
+    seed_int_var_bbfcedbeec = 6802
+    seed_int_var_aeacefeafa = 9266
+    seed_int_var_eebdbaebaf = 6974
+    seed_int_var_bbaeaffafb = 3710
+    x = n**2
+    squared_list = []
+    seed_int_var_aefbfdfbfe = 6633
+    seed_int_var_afeeffbbfa = 2630
+    seed_int_var_dabafdcbae = 6222
+    seed_int_var_deefacfbce = 929
+    my_dog = Dog("Fido", "brown", "Golden Retriever")
+    seed_int_var_bacaabecdc = 7153
+    my_cat = Cat("Whiskers", "gray")
+    seed_int_var_dfeeefaddf = 8555
+    my_person = Person("Bob")
+    my_person.JiⅉiJḬIｊǉｊĴÎiIĴIljillÎIḬǉⅉｊjǉⅉ(my_dog)
+    my_person.JiⅉiJḬIｊǉｊĴÎiIĴIljillÎIḬǉⅉｊjǉⅉ(my_cat)
+    my_person.ĴǉĲlijJḬⅉJĲlijIḬｊJḬǉⅉĴḬｊĳĴḬĲJI()
+    for m in range(x):
+        squared_list.append(i)
+    print("Error!")
+    if len(squared_list) % 2 == 0:
+        print("Yes!")
+        return True
+    else:
+        print("No!")
+        return False
+square_list(2, 15, 19, 11, 22, 61, 83, 49, 83)
+
+# Iterators
+my_iterator = iter(my_list)
+print(next(my_iterator))
+seed_int_var_cdbecaffee = 8128
+
+# Lambda functions
+add = lambda x, y: x + y
+seed_int_var_bdbbbcdbba = 3877
+print(add(2, 3))
+
+# Generators and iterables
+def squares(n, seed_int_param_fffecdebbf, seed_int_param_fadeabeaac, seed_int_param_edecddbcbe, seed_int_param_badebeddaf, seed_int_param_abcacccbbc, seed_int_param_aafdabbefd, seed_int_param_cfcfecebfa, seed_int_param_ddfeacdfee):
+    seed_int_var_facbdcbeae = 2218
+    seed_int_var_fffcaadbcd = 3841
+    seed_int_var_ccbccdacea = 9370
+    seed_int_var_debcadafaf = 3108
+    seed_int_var_aadafbebeb = 6861
+    seed_int_var_bbafcedbde = 4841
+    seed_int_var_bdacbbfcdb = 2627
+    for g in range(n):
+        yield g**2
+    seed_int_var_cdbbeecdcc = 2030
+
+my_generator = squares(5, 12, 96, 88, 4, 48, 64, 67, 25)
+for f in my_generator:
+    print(f)
+
+# Exceptions
+try:
+    a = 1 / 0
+except ZeroDivisionError:
+    print("Division by zero is not allowed.")
+seed_int_var_bcacfcdbbf = 4087
+
+# Context managers
+with open("test.txt", "w") as f:
+    f.write("Hello, world!")
+
+# Lambdas and higher-order functions
+def apply(func, x, seed_int_param_aeadbcfcbd, seed_int_param_deaedceeab, seed_int_param_ebfcaeceae, seed_int_param_faadfdfdbe, seed_int_param_bdbfefbdde, seed_int_param_aacbdeabfb, seed_int_param_aabcfdcafa, seed_int_param_cdfbcaaefb):
+    seed_int_var_aabfffafda = 282
+    return func(x)
+
+def test(h, x, seed_int_param_abacdbdccf, seed_int_param_edfdffbdbc, seed_int_param_effbffebaa, seed_int_param_bfdfdafffb, seed_int_param_fcbdcacfaa, seed_int_param_afffceceaf, seed_int_param_eddcbcfccb, seed_int_param_befaabfcce):
+    seed_int_var_bbbdddaeed = 5952
+    seed_int_var_fbecccaaec = 449
+    seed_int_var_ecffcefecc = 7697
+    seed_int_var_afdafefcdc = 518
+    seed_int_var_bfabcfaeba = 3654
+    seed_int_var_daddfdbccc = 4005
+    seed_int_var_cbbedcceef = 2773
+    seed_int_var_caaebbbfef = 4055
+    seed_int_var_ddadeaedec = 6708
+    seed_int_var_cbfadfbcbd = 550
+    seed_int_var_cddeeaadff = 1670
+    h += x
+    seed_int_var_bbedeaedda = 3610
+    return h
+    seed_int_var_cadfdddebe = 4267
+
+print(apply(lambda x: x**2, 3, 59, 96, 63, 40, 98, 6, 73, 18))
+x = 135743895743875
+y = 2
+z = 3
+i = (x + y) - (z + 12 & z) ^ 13
+j = i + x - z + 133
+print(j)
+print(i)
+def x(seed_int_param_aebebfcafd, seed_int_param_eccafedfed, seed_int_param_ffaeccedca, seed_int_param_ceeebafccc, seed_int_param_eabefeccdb, seed_int_param_cfabcbbddf, seed_int_param_dffcffdbbe, seed_int_param_eafbbbcffb):
+    seed_int_var_dbfaedcedf = 7405
+    seed_int_var_bbfdedaeef = 8121
+    seed_int_var_fbdcfebfdb = 1943
+    string = "abcdefghijklmnop"
+    seed_int_var_ccbeccedee = 7475
+    return string
+x(90, 58, 38, 76, 87, 24, 56, 1)
+
+if IS_OBFUSCATED:
+    print("This file is obfuscated!")
+else:
+    print("This file is not obfuscated.")
+    # This is a comment
+    # This is another comment
+    # This is yet another comment
+    # This is a final comment
+
+print(GET_HWID())
